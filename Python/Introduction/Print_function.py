62441f5618aab4aed658bb1e50e8553a708ec2bb
Abhishek Kukreja <46806402+abhi-kuks@users.noreply.github.com>
2020-07-08 14:24:19 +0530

    Create Print_function.py

---
+++ Python/Introduction/Print_function.py
@@ -0,0 +1,8 @@
+Question : https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    temp = ''
+    for i in range(1,n+1):
+        temp += f'{i}'
+    print(temp)
