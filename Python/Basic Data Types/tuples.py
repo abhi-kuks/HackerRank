b71b27de3be612cb1703d3cf60be242ff5db4469
Abhishek Kukreja <46806402+abhi-kuks@users.noreply.github.com>
2020-07-09 14:32:00 +0530

    Update tuples.py

--- Python/Basic Data Types/tuples.py
+++ Python/Basic Data Types/tuples.py
@@ -1,6 +1,6 @@
 Question : https://www.hackerrank.com/challenges/python-tuples/problem
 
-n = int()
+n = int(input())
 integer_list = map(int , input().split())
 t = tuple(integer_list)
 print(hash(t))
